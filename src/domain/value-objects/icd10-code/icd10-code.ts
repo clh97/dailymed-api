0fe4111f24448ad05e8347e237d1eeefc4afc37c
Michel Calheiros <michel@calheiros.dev>
2025-05-04 15:13:46 -0300

    chore: remove previous structure

--- src/domain/value-objects/icd10-code/icd10-code.ts
+++
@@ -1,1 +0,0 @@
-export class Icd10Code {}
--- src/modules/app.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DatabaseModule } from './database.module';
-import { ControllerModule } from './controller.module';
-
-@Module({
-  imports: [DatabaseModule, ControllerModule]
-})
-export class AppModule {}
